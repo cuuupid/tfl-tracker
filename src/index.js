a38205335daf4d9991fab1328c10e08b9dba9174
Priansh Shah <shah@priansh.com>
2017-12-18 04:59:50 -0500

    get data, using github as a cdn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var App = new Vue({
   el: '#app',
   data: function() {
     return {
+      stations: {},
       station: {},
       line: {},
       result: {}
@@ -11,5 +12,8 @@ var App = new Vue({
     update: function() {
       this.$forceUpdate()
     }
+  },
+  created: function() {
+    $.getJSON('', (json) => { this.stations = json })
   }
 })
